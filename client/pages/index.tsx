46633031b42ac8553d8f2d0eaec03a4e52e8c53d
Andrey Ohiienko <andreyohiienko@gmail.com>
2020-11-09 21:04:16 +0200

    Setup proxy

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,13 +1,29 @@
+import Axios from 'axios'
 import { PageLayout } from 'components'
 import { Activities, Approach, Hero, Services } from 'layouts'
+import React, { useEffect } from 'react'
 
-const IndexPage = () => (
-  <PageLayout title="Active Rest">
-    <Hero />
-    <Services />
-    <Activities />
-    <Approach />
-  </PageLayout>
-)
+const IndexPage = () => {
+  useEffect(() => {
+    const doRequest = async () => {
+      try {
+        const { data } = await Axios.get('http://localhost:3000/api')
+        console.log('data', data)
+      } catch (error) {
+        console.log('error', error)
+      }
+    }
+    doRequest()
+  }, [])
+
+  return (
+    <PageLayout title="Active Rest">
+      <Hero />
+      <Services />
+      <Activities />
+      <Approach />
+    </PageLayout>
+  )
+}
 
 export default IndexPage
--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,12 @@
-import express from 'express'
+import { json } from 'body-parser'
+import express, { Request, Response } from 'express'
 
 const app = express()
 
+app.get('/api', (req: Request, res: Response) => {
+  res.status(200).send({ hi: 'there' })
+})
+
 app.listen(5000, () => {
-  console.log('http://localhost:5000')
+  console.log('started server on http://localhost:5000')
 })
