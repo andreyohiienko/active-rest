3fc186a6ce85d2b6962cb0489991e3871d012572
Andrey Ohiienko <andreyohiienko@gmail.com>
2020-11-09 16:17:13 +0200

    Add carousel to hero

--- layouts/Hero/index.tsx
+++ layouts/Hero/index.tsx
@@ -1,23 +1,45 @@
 import { Container } from 'components'
-import { Button, Layout } from 'antd'
+import { Button, Carousel, Layout } from 'antd'
 import React from 'react'
 
 const Hero = () => {
+  const slides = [
+    {
+      title: 'Find yourself ourside.',
+      desc:
+        'Book unique camping experiences on over 300,000 campsites, cabins, RV parks, public parks and more.',
+      href: '/',
+      bg: 'https://picsum.photos/1920/1080',
+    },
+    {
+      title: 'Discover the nature.',
+      desc:
+        'Book unique camping experiences on over 300,000 campsites, cabins, RV parks, public parks and more.',
+      href: '/',
+      bg: 'https://picsum.photos/1920/1080',
+    },
+  ]
+
   return (
-    <Layout
-      className="hero bg-cover"
-      style={{ backgroundImage: 'url(https://picsum.photos/1920/1080)' }}
-    >
-      <Container className="text-center hero__container text-white text-capitalize">
-        <h1>Find yourself ourside.</h1>
-        <p>
-          Book unique camping experiences on over 300,000 campsites, cabins, RV
-          parks, public parks and more.
-        </p>
-        <Button href="/" size="large" shape="round" type="primary">
-          Discover
-        </Button>
-      </Container>
+    <Layout>
+      <Carousel effect="fade" draggable autoplay={false} dots={false}>
+        {slides.map(({ title, desc, href, bg }) => (
+          <div key={title}>
+            <div
+              className="hero bg-cover"
+              style={{ backgroundImage: `url(${bg})` }}
+            >
+              <Container className="text-center hero__container text-white text-capitalize">
+                <h1>{title}</h1>
+                <p>{desc}</p>
+                <Button href={href} size="large" shape="round" type="primary">
+                  Discover
+                </Button>
+              </Container>
+            </div>
+          </div>
+        ))}
+      </Carousel>
     </Layout>
   )
 }
