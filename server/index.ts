c65e840855dfc1ec4430a72f329f77468ba0f329
Andrey Ohiienko <andreyohiienko@gmail.com>
2020-12-13 00:01:35 +0200

    Change resolvers order (fix no data on pages)

--- server/index.ts
+++ server/index.ts
@@ -21,7 +21,7 @@ mongoose.connection
   .on('error', (error) => console.log('Error connecting to MongoLab:', error))
 
 // Provide resolver functions for your schema fields
-const resolvers = { ...Pages, ...Medias }
+const resolvers = { ...Medias, ...Pages }
 
 const server = new ApolloServer({
   typeDefs,
