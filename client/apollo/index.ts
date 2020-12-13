9612cdecab9a98a8ece3dbbdf868306d8694c4b2
Andrey Ohiienko <andreyohiienko@gmail.com>
2020-12-14 01:17:45 +0200

    Add graphQL debugger

--- client/apollo/index.ts
+++ client/apollo/index.ts
@@ -1,20 +1,36 @@
 import { useMemo } from 'react'
 import {
   ApolloClient,
+  ApolloLink,
   InMemoryCache,
   NormalizedCacheObject,
 } from '@apollo/client'
 import { createUploadLink } from 'apollo-upload-client'
+import { onError } from '@apollo/client/link/error'
 
 let apolloClient: ApolloClient<NormalizedCacheObject>
 
 function createApolloClient() {
+  const errorLink = onError(({ graphQLErrors, networkError }) => {
+    if (graphQLErrors)
+      graphQLErrors.map(({ message, locations, path }) =>
+        console.log(
+          `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+          locations,
+        ),
+      )
+
+    if (networkError) console.log(`[Network error]: ${networkError}`)
+  })
+
+  const uploadLink = createUploadLink({
+    uri: 'http://localhost:5000/graphql',
+    credentials: 'same-origin',
+  })
+
   return new ApolloClient({
     ssrMode: typeof window === 'undefined',
-    link: createUploadLink({
-      uri: 'http://localhost:5000/graphql',
-      credentials: 'same-origin',
-    }),
+    link: ApolloLink.from([errorLink, uploadLink]),
     cache: new InMemoryCache(),
   })
 }
