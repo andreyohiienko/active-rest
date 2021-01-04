635d28a82ae2a8cdc36b5fddbf17f79d79653ec7
Andrey Ohiienko <andreyohiienko@gmail.com>
2021-01-04 19:47:26 +0200

    Add useAdmin hook

---
+++ client/hooks/useAdmin.ts
@@ -0,0 +1,19 @@
+import { gql, useQuery } from '@apollo/client'
+const ADMIN = gql`
+  query Admin {
+    admin: currentUser {
+      name
+      googleId
+      role
+      email
+    }
+  }
+`
+
+export function useAdmin() {
+  const { data } = useQuery(ADMIN, {
+    fetchPolicy: 'cache-only',
+  })
+
+  return data?.admin.role.includes('admin')
+}
