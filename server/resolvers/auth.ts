8549b3b3d92643ca46b90015e19b3b89753a1211
Andrey Ohiienko <andreyohiienko@gmail.com>
2021-01-08 15:59:47 +0200

    Remove console.log

--- server/resolvers/auth.ts
+++ server/resolvers/auth.ts
@@ -3,7 +3,6 @@ import { IResolvers } from 'apollo-server-express'
 export const Auth: IResolvers = {
   Query: {
     currentUser: async (_, __, context) => {
-      console.log('context.req.user', context.req.user)
       return context.req.user
     },
     signout: async (_, __, context) => {
